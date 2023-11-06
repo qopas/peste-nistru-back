ed9b7f7431302d3ea0989251d756a3718cf4033f
qopas <grigoras.dumiitru@gmail.com>
2023-11-06 12:09:40 +0000

    Integrate DeeplAI to autochat translate

---
+++ src/main/java/org/example/deepL/TranslatorClass.java
@@ -0,0 +1,14 @@
+package org.example.deepL;
+
+import com.deepl.api.DeepLException;import com.deepl.api.TextResult;
+import com.deepl.api.Translator;
+public class TranslatorClass {
+    Translator translator;
+    public TranslatorClass() throws Exception {
+        String authKey = "48383f19-ae7b-b799-9c15-8fa8df7dfcc2:fx";
+        this.translator = new Translator(authKey);
+    }
+    public TextResult translateText(String input, String lang) throws DeepLException, InterruptedException {
+        TextResult result = this.translator.translateText(input, null, lang);
+        return result;
+    }}
